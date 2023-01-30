cecd5f8433f41f559702d9f8c1f146b82bc5dc63
Dariel <80236164+darielgaizta@users.noreply.github.com>
2023-01-30 18:55:42 +0700

    file encryption

--- cipher/utils/io.py
+++ cipher/utils/io.py
@@ -7,8 +7,6 @@ from pathlib import Path
 from django.conf import settings
 from cryptography.fernet import Fernet
 
-from . import vigenere
-
 ASCII = ''.join(chr(i) for i in range(512))
 
 def read_file_txt(filepath):
@@ -42,37 +40,6 @@ def decrypt(filename, key):
 	decrypted = fernet.decrypt(encrypted)
 	with open(filename, 'wb') as decrypted_file:
 		decrypted_file.write(decrypted)
-
-def extended_encrypt(filename, key):
-	encoded = b''
-
-	with open(filename, 'rb') as file:
-		file_in_bytes = file.read()
-
-	key = vigenere.generate_key(file_in_bytes, key)
-
-	for i in range(len(file_in_bytes)):
-		c = (file_in_bytes[i] + ord(key[i])) % 256
-		encoded += str(c).encode()
-
-	with open(filename, 'wb') as encrypted_file:
-		encrypted_file.write(encoded)
-
-
-def extended_decrypt(filename, key):
-	decoded = b''
-
-	with open(filename, 'rb') as file:
-		file_in_bytes = file.read()
-
-	key = vigenere.generate_key(file_in_bytes, key)
-
-	for i in range(len(file_in_bytes)):
-		p = (file_in_bytes[i] - ord(key[i])) % 256
-		decoded += str(p).encode()
-
-	with open(filename, 'wb') as decrypted_file:
-		decrypted_file.write(decoded)
 
 if __name__ == '__main__':
 	pass
